022ebc77018df3ea7a41f47ee6356cdd64dc38fc
ilRECh <ildar.rch@gmail.com>
2022-02-10 01:15:47 +0300

    Working on sending files

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -167,7 +167,7 @@ void Server::run()
 
 void Server::readerClients()
 {
-	timeval tm = {0, 1};
+	timeval tm = {0, 1000};
 	fd_set fdsCopy = _FdsSet;
 	if (select(_MaxFd + 1, &fdsCopy, NULL, NULL, &tm) < 0) {
 		throw std::runtime_error(std::string("Error: Select") + strerror(errno));
